12356be96ea438013f01ac0511267007027cac6e
Mart Mangus <mart@mangus.ee>
2011-11-15 15:02:04 +0300

    Little structure changes

--- lib.php
+++ lib.php
@@ -1,6 +1,16 @@
 <?php
 
+/**
+ * Moodle repository plugin for http://e-ope.ee/repositoorium
+ * @author Mart Mangus
+ */
+
 class repository_eope_repository extends repository {
+
+    private $listing = array(
+        'nologin' => true,
+        'dynload' => true
+    );
 
     public function __construct($repositoryid, $context = SITEID, $options = array()) {
         parent::__construct($repositoryid, $context, $options);
@@ -11,47 +21,79 @@ class repository_eope_repository extends repository {
         if (substr($filename, -4) != '.html')
             $filename .= '.html';
         $path = $this->prepare_file($filename);
-        $redirectcontent = file_get_contents($CFG->wwwroot.'/repository/eope_repository/repository-redirect.html');
+        $redirectcontent = file_get_contents($CFG->wwwroot . '/repository/eope_repository/repository-redirect.html');
         $redirectcontent = str_replace('{{URL}}', $url, $redirectcontent);
         file_put_contents($path, $redirectcontent);
         return array('path'=>$path, 'url'=>$url);
     }
 
+    /**
+     * @arg $path
+     * Possible paths:
+     *   all_entries/school_id=123/entry_id=123
+     *   my_entries/entry_id=123
+     *   search/search_string=findme/entry_id=123
+     */
     public function get_listing($path='', $page='') {
-        global $USER;
+        //global $USER;
         //$USER->email;
 
-        /*
-        if ($path == 'my_entries')
-            die('korras!'); 
-        */
+        if ($path == '') {
+            $this->listing_start();
+        } else {
+            $paths = explode('/', $path);
+            switch ($paths[0]) {
+                case 'all_entries':
+                    $this->listing_all($paths);
+                    break;
+                case 'my_entries':
+                    $this->listing_my($paths);
+                    break;                    
+                case 'search':
+                    $this->listing_search($paths);
+                    break;
+                default:
+                    throw new Exception("Error: Nothing available with this path: '$path'");
+            }
+        }
+        return $this->get_current_listing();
+    }
+
+    private function listing_start() {
+        $this->listing['path'] = array(
+            array('name' => get_string('repository', 'repository_eope_repository'),'path' => '/')
+        );
 
         $itemslist = array(
+            array('title' => get_string('my_entries', 'repository_eope_repository'),
+                'path' => 'my_entries',
+                'thumbnail' => 'https://h1.moodle.e-ope.ee/theme/image.php?theme=anomaly&image=f%2Ffolder-32&rev=217#TODO-FIX-THIS',
+                'children' => array()),
+            array('title' => get_string('all_entries', 'repository_eope_repository'),
+                'path' => 'all_entries',
+                'thumbnail' => 'https://h1.moodle.e-ope.ee/theme/image.php?theme=anomaly&image=f%2Ffolder-32&rev=217#TODO-FIX-THIS',
+                'children' => array())
+        );
+        $this->listing['list'] = $itemslist;
+    }
+
+    private function listing_all($paths) {
+        //TODO
             /*
             array('title'=>'Test File.zip .html',
                 'thumbnail'=>'https://h1.moodle.e-ope.ee/theme/image.php?theme=anomaly&image=icon&rev=217&component=repository_eope_repository#TODO-FIX-THIS',
                 'source'=>'http://e-ope.ee/_download/euni_repository/file/821/kameerika.zip'),
             */
-            array('title'=>get_string('my_entries', 'repository_eope_repository'),
-                'path'=>'my_entries',
-                'thumbnail'=>'https://h1.moodle.e-ope.ee/theme/image.php?theme=anomaly&image=f%2Ffolder-32&rev=217#TODO-FIX-THIS',
-                'children'=>array()),
-            array('title'=>get_string('all_entries', 'repository_eope_repository'),
-                'path'=>'all_entries',
-                'thumbnail'=>'https://h1.moodle.e-ope.ee/theme/image.php?theme=anomaly&image=f%2Ffolder-32&rev=217#TODO-FIX-THIS',
-                'children'=>array())
-        );
+    }
+    private function listing_my($paths) {
+        //TODO
+    }
+    private function listing_search($paths) {
+        //TODO
+    }
 
-        $list = array(
-            'nologin'=>true,
-            'dynload'=>true,
-             //this will be used to build navigation bar
-            'path'=>array(
-                array('name'=>get_string('repository', 'repository_eope_repository'),'path'=>'/')
-            ),
-            'list'=> $itemslist
-        );
-        return $list;
+    private function get_current_listing() {
+        return $this->listing;
     }
 
     /* TODO
